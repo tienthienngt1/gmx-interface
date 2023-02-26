5cd4b4e15dfedd2740570dee6ef108e5f482e864
tienthienngt1 <tienthienngt2@gmail.com>
2023-02-26 16:33:49 +0700

    fix some link gitbook

--- src/pages/Referrals/Referrals.js
+++ src/pages/Referrals/Referrals.js
@@ -128,7 +128,10 @@ function Referrals({ connectWallet, setPendingTxns, pendingTxns }) {
                 Get fee discounts and earn rebates through the Optimoon referral program.
                 <br />
                 For more information, please read the{" "}
-                <ExternalLink href="https://gitbook.io">referral program details</ExternalLink>.
+                <ExternalLink href="https://optimoon.gitbook.io/optimoon/referrals">
+                  referral program details
+                </ExternalLink>
+                .
               </Trans>
             </div>
           </div>
