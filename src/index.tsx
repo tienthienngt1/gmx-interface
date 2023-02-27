cb012c5cd93e63b096f56e5f90e45873c68d120d
tienthienngt1 <tienthienngt2@gmail.com>
2023-02-27 23:02:22 +0700

    clear 404 pages

--- src/index.tsx
+++ src/index.tsx
@@ -7,10 +7,10 @@ import reportWebVitals from "./reportWebVitals";
 
 ReactDOM.render(
   <React.StrictMode>
-    <>404 NOT FOUND</>
-    {/* <Router>
+    {/* <>404 NOT FOUND</> */}
+    <Router>
       <App />
-    </Router> */}
+    </Router>
   </React.StrictMode>,
   document.getElementById("root")
 );
